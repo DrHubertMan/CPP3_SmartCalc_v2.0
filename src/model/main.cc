682a27e6dc3b990528749a8e86f6bf34f0f541b2
mammiemi_home <mammiemi@student.21-school.ru>
2023-08-14 22:35:11 +0300

    for debug

--- src/model/main.cc
+++ src/model/main.cc
@@ -2,8 +2,11 @@
 #include "expressionConverter.h"
 
 int main() {
-    std::list<std::string> input_line = {"5","*", "6", "+", "(", "2", "-", "9", ")"};
+    std::list<std::string> input_line = {"5","*", "6", "+", "2", "-", "9"};
     s21::expressionConverter Convert(input_line);
+    int a = 1;
+    int b = 2;
+    int c = 3;
     Convert.Conversion();
     std::vector<std::string> output = Convert.GetOut();
     for (auto &item: output) {
