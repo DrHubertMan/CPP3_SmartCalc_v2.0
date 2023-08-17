95371d71ae969d983129acf3d77d0b2e2f68ee3c
root <root@vm3848168.25ssd.had.wf>
2023-08-17 16:08:45 +0300

    fix main for test

--- src/model/main.cc
+++ src/model/main.cc
@@ -5,7 +5,7 @@ int main() {
     std::list<std::string> input_line = {"sin", "(", "2", "^", "3", "^", "4", ")"};
     s21::ExpressionConverter Convert(input_line);
     s21::ExpressionConverter lola(Convert);
-    std::vector<std::string> output = lola.GetOut();
+    std::list<std::string> output = lola.GetOut();
     for (auto &item: output) {
         std::cout << item;
     }
