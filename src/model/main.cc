9a5afdf0605b7224e65165c1c0950844562cf51f
root <root@vm3848168.25ssd.had.wf>
2023-08-17 15:20:06 +0300

    change for tests

--- src/model/main.cc
+++ src/model/main.cc
@@ -3,9 +3,9 @@
 
 int main() {
     std::list<std::string> input_line = {"sin", "(", "2", "^", "3", "^", "4", ")"};
-    s21::expressionConverter Convert(input_line);
-    Convert.Conversion();
-    std::vector<std::string> output = Convert.GetOut();
+    s21::ExpressionConverter Convert(input_line);
+    s21::ExpressionConverter lola(Convert);
+    std::vector<std::string> output = lola.GetOut();
     for (auto &item: output) {
         std::cout << item;
     }
