a2deef11ad7e5ba8dae473b29053d607074c430f
mammiemi_home <mammiemi@student.21-school.ru>
2023-08-27 13:41:26 +0300

    feat: add in calculation class all calculation cases

--- src/model/calculation.cc
+++ src/model/calculation.cc
@@ -1,16 +1,16 @@
 #include "calculation.h"
 
-using s21::Calculation;
-
-Calculation::Calculation(std::list<std::string> input) : input_string_(input), value_(0) {
+s21::Calculation::Calculation(std::list<std::string> input)
+    : input_string_(input), value_(0) {
   Calc();
 };
 
-Calculation::Calculation(const Calculation &c) : input_string_(c.input_string_), value_(c.value_) {
+s21::Calculation::Calculation(const Calculation &c)
+    : input_string_(c.input_string_), value_(c.value_) {
   Calc();
 };
 
-Calculation& Calculation::operator=(const Calculation &c) {
+s21::Calculation &s21::Calculation::operator=(const Calculation &c) {
   if (this != &c) {
     Calculation copy{c};
     swap(copy);
@@ -18,39 +18,60 @@ Calculation& Calculation::operator=(const Calculation &c) {
   return *this;
 };
 
-Calculation::Calculation(Calculation &&c) : Calculation() {
+s21::Calculation::Calculation(Calculation &&c) : Calculation() {
   swap(c);
   Calc();
 };
 
-Calculation& Calculation::operator=(Calculation &&c) {
+s21::Calculation &s21::Calculation::operator=(Calculation &&c) {
   if (this != &c) {
     Calculation moved{std::move(c)};
     swap(moved);
   }
   return *this;
+}
+double s21::Calculation::GetValue() const noexcept { return value_; };
 };
 
-void Calculation::swap(Calculation &other) {
+void s21::Calculation::swap(Calculation &other) {
   std::swap(input_string_, other.input_string_);
   std::swap(value_, other.value_);
 };
 
-void Calculation::Calc() noexcept {
+void s21::Calculation::Calc() noexcept {
   std::stack<double> calc_stack;
   calc_stack.push(value_);
   while (!input_string_.empty()) {
-    std::string token = input_string_.front();
+    Element token(input_string_.front());
     input_string_.pop_front();
-    if (IsOperand(token)) {
+    if (token.IsNumber()) {
       AddOperandInStack(token, calc_stack);
-    } else if (IsPlus(token)) {
+    } else if (token.IsPlus()) {
       PlusCase(calc_stack);
+    } else if (token.IsMinus()) {
+      MinusCase(calc_stack);
+    } else if (token.IsMul()) {
+      MulCase(calc_stack);
+    } else if (token.IsDiv()) {
+      DivCase(calc_stack);
+    } else if (token.IsExp()) {
+      ExpCase(calc_stack);
+    } else if (token.IsMod()) {
+      ModCase(calc_stack);
+    } else if (token.IsFunciotn()) {
+      FuncCase(calc_stack, token);
     }
   }
+  value_ = calc_stack.top();
 };
 
-void Calculation::PlusCase(std::stack<double> calc_stack) noexcept {
+void s21::Calculation::AddOperandInStack(
+    Element &token, std::stack<double> &calc_stack) noexcept {
+  double num = std::stod(token.GetData());
+  calc_stack.push(num);
+};
+
+void s21::Calculation::PlusCase(std::stack<double> &calc_stack) noexcept {
   double val_1 = calc_stack.top();
   calc_stack.pop();
   double val_2 = calc_stack.top();
@@ -58,64 +79,119 @@ void Calculation::PlusCase(std::stack<double> calc_stack) noexcept {
   calc_stack.push(val_2 + val_1);
 }
 
-void Calculation::AddOperandInStack(std::string token, std::stack<double> calc_stack) noexcept {
-  double num = std::stod(token);
-  calc_stack.push(num);
+void s21::Calculation::MinusCase(std::stack<double> &calc_stack) noexcept {
+  double val_1 = calc_stack.top();
+  calc_stack.pop();
+  double val_2 = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(val_2 - val_1);
 };
 
-bool Calculation::IsPlus(std::string token) noexcept {
-  return (token[0] == '+');
+void s21::Calculation::MulCase(std::stack<double> &calc_stack) noexcept {
+  double val_1 = calc_stack.top();
+  calc_stack.pop();
+  double val_2 = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(val_2 * val_1);
 };
 
-bool Calculation::IsOperand(std::string token) noexcept {
-  return ((token[0] >= '0' && token[0] <= 9) || (token[0] == '-' && token[1] >= '0' && token[1] <= 9));
+void s21::Calculation::DivCase(std::stack<double> &calc_stack) noexcept {
+  double val_1 = calc_stack.top();
+  calc_stack.pop();
+  double val_2 = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(val_2 / val_1);
 };
 
-bool Calculation::IsMinus(std::string token) noexcept {
-  return (token[0] == '-');
+void s21::Calculation::ExpCase(std::stack<double> &calc_stack) noexcept {
+  double val_1 = calc_stack.top();
+  calc_stack.pop();
+  double val_2 = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(pow(val_2, val_1));
 };
 
-void Calculation::MinusCase(std::stack<double> calc_stack) noexcept{};
-
-bool Calculation::IsMul(std::string token) noexcept {
-  return (token[0] == '*');
+void s21::Calculation::ModCase(std::stack<double> &calc_stack) noexcept {
+  double val_1 = calc_stack.top();
+  calc_stack.pop();
+  double val_2 = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(static_cast<int>(val_2) % static_cast<int>(val_1));
 };
 
-void Calculation::MulCase(std::stack<double> calc_stack) noexcept {};
-
-bool Calculation::IsDiv(std::string token) noexcept {
-  return (token[0] == '/');
+void s21::Calculation::FuncCase(std::stack<double> &calc_stack,
+                                Element &token) noexcept {
+  if (token.IsSin()) {
+    SinCase(calc_stack);
+  } else if (token.IsCos()) {
+    CosCase(calc_stack);
+  } else if (token.IsTan()) {
+    TanCase(calc_stack);
+  } else if (token.IsAsin()) {
+    AsinCase(calc_stack);
+  } else if (token.IsAcos()) {
+    AcosCase(calc_stack);
+  } else if (token.IsAtan()) {
+    AtanCase(calc_stack);
+  } else if (token.IsSqrt()) {
+    SqrtCase(calc_stack);
+  } else if (token.IsLn()) {
+    LnCase(calc_stack);
+  } else if (token.IsLog()) {
+    LogCase(calc_stack);
+  }
 };
-void Calculation::DivCase(std::stack<double> calc_stack) noexcept {};
 
-bool Calculation::IsExp(std::string token) noexcept {
-  return (token[0] == '^');
-};
-void Calculation::ExpCase(std::stack<double> calc_stack) noexcept {};
-
-bool Calculation::IsMod(std::string token) noexcept {
-  return (token[0] == '%');
+void s21::Calculation::SinCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(sin(val));
 };
 
-void Calculation::ModCase(std::stack<double> calc_stack) noexcept {};
+void s21::Calculation::CosCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(cos(val));
+};
 
-bool Calculation::IsFunciotn(std::string token) noexcept {};
-void Calculation::FuncCase(std::stack<double> calc_stack, std::string token) noexcept {};
-bool Calculation::IsSin(std::string token) noexcept {};
-void Calculation::SinCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsCos(std::string token) noexcept {};
-void Calculation::CosCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsTan(std::string token) noexcept {};
-void Calculation::TanCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsAsin(std::string token) noexcept {};
-void Calculation::AsinCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsAcos(std::string token) noexcept {};
-void Calculation::AcosCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsAtan(std::string token) noexcept {};
-void Calculation::AtanCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsSqrt(std::string token) noexcept {};
-void Calculation::SqrtCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsLn(std::string token) noexcept {};
-void Calculation::LnCase(std::stack<double> calc_stack) noexcept {};
-bool Calculation::IsLog(std::string token) noexcept {};
-void Calculation::LogCase(std::stack<double> calc_stack) noexcept {};
+void s21::Calculation::TanCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(tan(val));
+};
+
+void s21::Calculation::AsinCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(asin(val));
+};
+
+void s21::Calculation::AcosCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(acos(val));
+};
+
+void s21::Calculation::AtanCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(atan(val));
+};
+
+void s21::Calculation::SqrtCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(sqrt(val));
+};
+
+void s21::Calculation::LnCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(log(val));
+};
+
+void s21::Calculation::LogCase(std::stack<double> &calc_stack) noexcept {
+  double val = calc_stack.top();
+  calc_stack.pop();
+  calc_stack.push(log10(val));
+};
--- src/model/calculation.h
+++ src/model/calculation.h
@@ -1,13 +1,16 @@
 #ifndef CPP3_SMARTCALC_V2_1_CALCULATION_H
 #define CPP3_SMARTCALC_V2_1_CALCULATION_H
 
-#include <string>
 #include <list>
 #include <stack>
+#include <string>
+#include <cmath>
+
+#include "element.h"
 
 namespace s21 {
 class Calculation {
-  public:
+public:
   Calculation() = default;
   ~Calculation() = default;
 
@@ -19,47 +22,33 @@ class Calculation {
   Calculation(Calculation &&c);
   Calculation &operator=(Calculation &&c);
 
-  private:
+private:
   std::list<std::string> input_string_;
   double value_;
 
-  private:
+private:
   void Calc() noexcept;
   void swap(Calculation &other);
-  bool IsOperand(std::string token) noexcept;
-  void AddOperandInStack(std::string token, std::stack<double> calc_stack) noexcept;
-  bool IsPlus(std::string token) noexcept;
-  void PlusCase(std::stack<double> calc_stack) noexcept;
-  bool IsMinus(std::string token) noexcept;
-  void MinusCase(std::stack<double> calc_stack) noexcept;
-  bool IsMul(std::string token) noexcept;
-  void MulCase(std::stack<double> calc_stack) noexcept;
-  bool IsDiv(std::string token) noexcept;
-  void DivCase(std::stack<double> calc_stack) noexcept;
-  bool IsExp(std::string token) noexcept;
-  void ExpCase(std::stack<double> calc_stack) noexcept;
-  bool IsMod(std::string token) noexcept;
-  void ModCase(std::stack<double> calc_stack) noexcept;
-  bool IsFunciotn(std::string token) noexcept;
-  void FuncCase(std::stack<double> calc_stack, std::string token) noexcept;
-  bool IsSin(std::string token) noexcept;
-  void SinCase(std::stack<double> calc_stack) noexcept;
-  bool IsCos(std::string token) noexcept;
-  void CosCase(std::stack<double> calc_stack) noexcept;
-  bool IsTan(std::string token) noexcept;
-  void TanCase(std::stack<double> calc_stack) noexcept;
-  bool IsAsin(std::string token) noexcept;
-  void AsinCase(std::stack<double> calc_stack) noexcept;
-  bool IsAcos(std::string token) noexcept;
-  void AcosCase(std::stack<double> calc_stack) noexcept;
-  bool IsAtan(std::string token) noexcept;
-  void AtanCase(std::stack<double> calc_stack) noexcept;
-  bool IsSqrt(std::string token) noexcept;
-  void SqrtCase(std::stack<double> calc_stack) noexcept;
-  bool IsLn(std::string token) noexcept;
-  void LnCase(std::stack<double> calc_stack) noexcept;
-  bool IsLog(std::string token) noexcept;
-  void LogCase(std::stack<double> calc_stack) noexcept;
+
+  void AddOperandInStack(Element &token,
+                         std::stack<double> &calc_stack) noexcept;
+
+  void PlusCase(std::stack<double> &calc_stack) noexcept;
+  void MinusCase(std::stack<double> &calc_stack) noexcept;
+  void MulCase(std::stack<double> &calc_stack) noexcept;
+  void DivCase(std::stack<double> &calc_stack) noexcept;
+  void ExpCase(std::stack<double> &calc_stack) noexcept;
+  void ModCase(std::stack<double> &calc_stack) noexcept;
+  void FuncCase(std::stack<double> &calc_stack, Element &token) noexcept;
+  void SinCase(std::stack<double> &calc_stack) noexcept;
+  void CosCase(std::stack<double> &calc_stack) noexcept;
+  void TanCase(std::stack<double> &calc_stack) noexcept;
+  void AsinCase(std::stack<double> &calc_stack) noexcept;
+  void AcosCase(std::stack<double> &calc_stack) noexcept;
+  void AtanCase(std::stack<double> &calc_stack) noexcept;
+  void SqrtCase(std::stack<double> &calc_stack) noexcept;
+  void LnCase(std::stack<double> &calc_stack) noexcept;
+  void LogCase(std::stack<double> &calc_stack) noexcept;
 };
-};
+}; // namespace s21
 #endif
