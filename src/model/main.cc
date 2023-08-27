12f14342d9862cf27e752c8d1c63e281f0e8c97f
mammiemi_home <mammiemi@student.21-school.ru>
2023-08-27 13:41:44 +0300

    in main add havy case

--- src/model/main.cc
+++ src/model/main.cc
@@ -2,7 +2,7 @@
 #include "expressionConverter.h"
 
 int main() {
-    std::list<std::string> input_line = {"sin", "(", " 2", "*", "(", "3", "+", "1", ")", ")"};
+    std::list<std::string> input_line = {"3", "+", "sin", "(", "2.3", ")", "*", "(", "4", "-", "7", ")", "^", "2", "+", "cos", "(", "2", ")", "/", "tan", "(", "5", ")", "*", "8"};
     s21::ExpressionConverter Convert(input_line);
     s21::ExpressionConverter lola(Convert);
     std::list<std::string> output = lola.GetOut();
